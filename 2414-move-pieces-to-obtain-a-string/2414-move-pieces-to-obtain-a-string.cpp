1fa40b27cf8fded17802369c0b6fd8aada088e5f
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2024-12-05 22:00:02 +0500

    Time: 8 ms (71.26%), Space: 21.9 MB (33.33%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -3,41 +3,37 @@ public:
 
     bool canChange(string start, string target) {
 
-        // char a=t[0];
-        // int j=0;
-        // while(j<t.size()and t[j]!='L' and t[j]!='R') 
-        // {   
-        //     a=t[j];
-        //     j++;
-        // }
+        int left=0, right=0;
 
-        // for(int i=0;i<s.size();i++)
-        // {
-        //     if()
-        // }
-
-
-        // return false;
-
-        int left = 0, right = 0;
-
-        for (int i = 0; i < start.size(); i++) {
-            if (start[i] == 'R') {
+        for(int i=0;i<start.size();i++)
+        {
+            if(start[i]=='R')
+            {
                 right++;
-                if (left != 0)
-                    return false;
-            } else if (start[i] == 'L')
-                left--;
-            if (target[i] == 'R')
-                right--;
-            else if (target[i] == 'L') {
-                left++;
-                if (right != 0)
+                if(left!=0)
                     return false;
             }
+            else if(start[i]=='L')
+            {
+                left--;
+            }
+
+            if(target[i]=='R')
+            {
+                right--;
+                if(left!=0) return false;
+            }
+            else if( target[i]=='L')
+            {
+                left++;
+                if(right!=0) return false;
+            }
+
             if (left < 0 || right < 0)
                 return false;
         }
-        return left == 0 and right == 0;
+        
+
+        return left==0 and right==0;
     }
 };
