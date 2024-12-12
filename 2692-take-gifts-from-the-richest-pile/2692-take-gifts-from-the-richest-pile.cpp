3d306b8bbf0929177e576d771a86df16cc18710d
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2024-12-12 18:37:20 +0500

    Time: 0 ms (100%), Space: 13.3 MB (16.26%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
         
+        priority_queue<int> pq(gifts.begin(),gifts.end());
+    
         for(int i=1;i<=k;i++)
         {
-            auto it=max_element(gifts.begin(),gifts.end());
-            int m=*max_element(gifts.begin(),gifts.end());
-            *it=sqrt(m);
+            int t=pq.top();
+            pq.pop();
+            pq.push(sqrt(t));
         }
-
-        long long int s=0;
-        for(int i=0;i<gifts.size();i++)
-            s+=gifts[i];
-        return s;
+        
+        long long sum=0;
+        while(!pq.empty())
+        {
+            sum+=pq.top();
+            pq.pop();
+        }
+        return sum;
 
     }
 };
