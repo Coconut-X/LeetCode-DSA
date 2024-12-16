b70aea49f42bad7e16974f1fd8925073daa9d524
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2024-12-16 12:10:47 +0500

    Time: 0 ms (100%), Space: 27.2 MB (45.93%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -4,11 +4,8 @@ public:
         
         for(int i=1;i<=k;i++)
         {
-        //     nums[distance(nums.begin(),min_element(nums.begin(),nums.end()))]=*min_element(nums.begin(),nums.end())*multiplier;
-        // }
-
-        auto it=min_element(nums.begin(),nums.end());
-        *it=*it*multiplier;
+            auto it=min_element(nums.begin(),nums.end());
+            *it=*it*multiplier;
         }
 
         return nums;
