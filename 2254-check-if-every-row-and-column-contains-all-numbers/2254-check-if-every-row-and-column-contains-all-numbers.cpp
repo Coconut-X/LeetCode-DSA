36e68d2c6c2b5803d9efc3b81e6ca8e29cbaf1f4
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2024-12-16 19:40:07 +0500

    Time: 186 ms (10.33%), Space: 99.1 MB (9.88%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
+    
     int n=matrix.size();
-
     int sum=(n*(n+1))/2;
     unordered_map<int,int>mrow[n], mcol[n];
     vector<int> row(n,0), col(n,0);
@@ -13,23 +13,9 @@ public:
         {
             if(mrow[i][matrix[i][j]] or mcol[j][matrix[i][j]] ) return false;
             mrow[i][matrix[i][j]]=1, mcol[j][matrix[i][j]]=1;
-            //row[i]+=matrix[i][j];
-            //col[j]+=matrix[i][j];
-
         }
     }
-
+    
     return true;
-
-    for(int i=0;i<n;i++)
-        if(row[i]!=sum or col[i]!=sum) return false;
-    return true;
-
-
-    // vector<int> row(n,0), col(n,0);
-
-
-    // for(int i=0;i<)
-        
     }
 };
