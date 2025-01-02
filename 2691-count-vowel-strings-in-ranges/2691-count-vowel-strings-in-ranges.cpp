989fb436a3d8cabcaf80ec9937a7dde6d1cc447d
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-02 16:01:34 +0500

    Time: 52 ms (10.5%), Space: 81.5 MB (5.16%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -4,7 +4,7 @@ public:
 
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
 
-        unordered_map<int,int> m;
+        unordered_map<int,int> prefix;
 
         auto vowel=[](char curr){
             return curr == 'a' or curr == 'e' or curr == 'i' or curr == 'o' or curr == 'u';
@@ -14,21 +14,14 @@ public:
         for(int i=0;i<words.size();i++)
         {
             string curr=words[i];
-            m[i]=((vowel(curr[0]) and vowel(curr[curr.size()-1]))? ++count: count);
+            prefix[i]=((vowel(curr[0]) and vowel(curr[curr.size()-1]))? ++count: count);
         }
 
         vector<int> ans;
-        for(int i=0; i<queries.size();i++)
-        {
-            int s=queries[i][0];
-            int e=queries[i][1];
-
-            int sum=m[e]-m[s-1];
-           
-            ans.push_back(sum);
-        }
+        
+        for(auto q:queries)
+            ans.push_back(prefix[q[1]]-prefix[q[0]-1]);
 
         return ans;
-        
     }
 };
