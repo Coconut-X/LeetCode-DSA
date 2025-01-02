a3c05ef4e43363a004d25e5def0a0a2c08a12ca4
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-03 00:05:36 +0500

    Time: 2 ms (24.97%), Space: 10.4 MB (5.41%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     int maxScore(string s) {
         
-        unordered_map<int,int> zeros;
-        unordered_map<int,int> ones;
+        unordered_map<int,int> zeros, ones;
 
-        int n=s.size();
-
-        int left=0;
-        int right=0;
-        int m=0;
+        int n=s.size(), left=0, right=0, m=0;
         for(int i=0, j=n-1;i<n;i++,j--)
         {
-            if(s[i]=='0') left++;
-            if(s[j]=='1') right++;
-            zeros[i]=left;
-            ones[j]=right;
+            left+=s[i]=='0', right+=s[j]=='1';
+            zeros[i]=left, ones[j]=right;
         }
 
         for(int k=0;k<n-1;k++)
@@ -24,6 +17,5 @@ public:
         }
 
         return m;
-
     }
 };
