1698defcf03f4ee69077bc5fc78527e5e20e5634
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-03 17:39:57 +0500

    Time: 152 ms (5.25%), Space: 160.7 MB (6.2%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -4,22 +4,13 @@ public:
         unordered_map<long long,long long> left;
         long long l=0;
         for(int i=0;i<nums.size();i++)
-        {
-            l+=nums[i];
-            left[i]=l;
-        }
+            left[i]+=( i==0? nums[i]: left[i-1]+nums[i]);
 
         int count=0;
         for(int i=0;i<nums.size()-1;i++)
-        {
-            if(left[i]>= left[nums.size()-1] - left[i] )
-            {
+            if(left[i]>= left[nums.size()-1] - left[i])
                 count++;
-            }
-        }
-
 
         return count;
-        
     }
 };
