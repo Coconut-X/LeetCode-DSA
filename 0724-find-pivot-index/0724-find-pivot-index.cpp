3cced77a9b6ca66dbbac967329b86e4f948472c2
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-05 01:26:50 +0500

    Time: 23 ms (14.11%), Space: 42.5 MB (9.05%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        unordered_map<int,int> sums;
+
+        for(int i=0;i<nums.size();i++)
+            sums[i]+=(i==0?nums[i]: nums[i]+sums[i-1]);
+        
+        for(int i=0;i<nums.size();i++)
+            if(sums[i-1]==sums[nums.size()-1]- sums[i])
+                return i;
+        return -1;
+        
+    }
+};
