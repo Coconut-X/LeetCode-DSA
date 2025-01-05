352c28425e9dacc21eb62ac20f2cd2b662bf261e
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-05 13:16:09 +0500

    Time: 0 ms (100%), Space: 10.9 MB (29.45%) - LeetHub

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+
+        int h=0, s=0;
+        for(int i=0;i<gain.size();i++)
+            h=max(h,s+=gain[i]);
+
+        return h;
+        
+    }
+};
