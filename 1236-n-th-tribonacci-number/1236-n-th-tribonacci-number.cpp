811ee099193d57aab438d15374d29f4f8f5af989
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-09 01:06:09 +0500

    Time: 0 ms (100%), Space: 8.2 MB (28.08%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-
     unordered_map<int,int> tri;
 
     int tribonacci(int n) {
-        
         if(n<=2) return n!=0;
-        if(tri[n]) return tri[n];
-        tri[n]=tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
-        return tri[n];
+        return (tri[n]? tri[n]: tri[n]=tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3) );
     }
 };
