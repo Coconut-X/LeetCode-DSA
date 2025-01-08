57d27435e1cac7c0446ead63da485439027e478c
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-08 11:57:08 +0500

    Time: 0 ms (100%), Space: 23.5 MB (66.95%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -5,9 +5,9 @@ public:
     {
         if(str1.size() > str2.size())
             return false;
-        int j=0;        
+                
         int s1=str1.size(), s2=str2.size();
-        for(int i=0;i<str2.size() and j<s1;i++)
+        for(int i=0, j=0; i<str2.size() and j<s1;i++)
         {
             if(str2[i]!=str1[j] or str1[s1-1-j]!=str2[s2-1-i])
                 return false;
@@ -19,19 +19,11 @@ public:
 
     int countPrefixSuffixPairs(vector<string>& words) {
         
-       // unordered_map<pair<string,string>, bool> m;
         int count=0;
         for(int i=0;i<words.size()-1;i++)
-        {
             for(int j=i+1;j<words.size();j++)
-            {
                 if( isPrefixAndSuffix(words[i],words[j]))
-                {
                     count++;
-                    //map[{words[i],words[j]}]=true;
-                }  
-            }
-        }
 
         return count;
     }
