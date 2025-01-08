b1d0f184f1593778f3d07c7a87308446da85bf59
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-08 23:30:09 +0500

    Time: 212 ms (5.21%), Space: 200.1 MB (9.59%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
 
-        int window=k;
-
         unordered_map<int,double> sum;
+        double m=INT_MIN;  
 
         for(int i=0;i<nums.size();i++)
             sum[i]=(i==0? nums[i]: sum[i-1]+nums[i]);
 
-        double m=INT_MIN;  
         for(int i=0;i+k - 1<nums.size();i++)
-        {
-            m=max(m, (sum[i + k - 1] - sum[i-1])/k);
-        }
+            m=max(m, (sum[i + k - 1] - sum[i-1]));
 
-        return m;
+        return m/k;
         
     }
 };
