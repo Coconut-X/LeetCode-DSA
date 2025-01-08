a35cc2b934643def397038421f50c847419ec4d9
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-09 01:04:19 +0500

    Time: 0 ms (100%), Space: 8.3 MB (26.24%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -5,11 +5,8 @@ public:
 
     int tribonacci(int n) {
         
-        if(n==0) return 0;
-        if(n==1 or n==2) return 1;
-
+        if(n<=2) return n!=0;
         if(tri[n]) return tri[n];
-    
         tri[n]=tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
         return tri[n];
     }
