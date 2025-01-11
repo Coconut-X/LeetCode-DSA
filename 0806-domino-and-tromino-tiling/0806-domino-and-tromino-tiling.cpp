304a5b7698a2f1576c9e91064a9f45b99b93fda5
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-01-11 14:47:00 +0500

    Time: 0 ms (100%), Space: 7.8 MB (77.04%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -8,13 +8,17 @@ public:
         dp[1]=1;
         dp[2]=2;
         dp[3]=5;
+        int a=1, b=2, c=5, ans;
 
-        if(n<=3) return dp[n];
+        if(n==1) return 1; if(n==2) return 2;if(n==3) return 5;
 
         for(int i=4;i<=n;i++)
-            dp[i]=(2*dp[i-1]+dp[i-3])%MOD;
+            //dp[i]=(2*dp[i-1]+dp[i-3])%MOD;
+        {
+            ans=((2*c)%MOD+a)%MOD, a=b, b=c, c=ans;
+        }
 
-        return dp[n];
+        return ans;
         
     }
 };
