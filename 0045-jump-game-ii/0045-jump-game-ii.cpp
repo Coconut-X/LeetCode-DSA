20db9fa16a0505a9364bc4604fc077417f2edb72
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-03-08 11:56:20 +0500

    Time: 337 ms (5.07%), Space: 22.5 MB (7.78%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
 
+
+    vector<int> mep;
+    int minJump(vector<int>&nums, int s, int i){
+        if(i>=s-1){
+            return 0;
+        }
+
+        if(nums[i]==0) return 10001;
+
+        if (mep[i]!=10001) return mep[i];
+
+        for(int k = 1; k<=nums[i];k++)
+        {
+            mep[i] = min(mep[i],1 + minJump(nums, s, i + k));
+        }
+
+        return mep[i];
+    }
+
+
     int jump(vector<int>& nums) {
-	vector<int> dp(size(nums), 10001); // initialise all to max possible jumps + 1 denoting dp[i] hasn't been computed yet
-	return solve(nums, dp, 0);
-}
-// recursive solver to find min jumps to reach end
-int solve(vector<int>& nums, vector<int>& dp, int pos) {
-	if(pos >= size(nums) - 1) return 0;    // when we reach end, return 0 denoting no more jumps required
-	if(dp[pos] != 10001) return dp[pos];    // number of jumps from pos to end is already calculated, so just return it
-	// explore all possible jump sizes from current position. Store & return min jumps required
-	for(int j = 1; j <= nums[pos]; j++)
-		dp[pos] = min(dp[pos], 1 + solve(nums, dp, pos + j));        
-	return dp[pos];
-}
+
+        mep.resize(nums.size(),10001);
+        return minJump(nums, nums.size(), 0);
+        
+    }
 };
