cea7a392eec8062049b05b4b18979022b6a7cbc7
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-03-15 00:44:53 +0500

    Time: 24 ms (71.51%), Space: 88.3 MB (7.72%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    bool canDistribute(vector<int>& candies, long long k, int mid) {
-        long long count = 0;
-        for (int c : candies) {
-            count += c / mid;  // Count how many children can receive `mid` candies
-            if (count >= k) return true;  // If enough children are served, return true
-        }
-        return false;
-    }
-
     int maximumCandies(vector<int>& candies, long long k) {
-        if (accumulate(candies.begin(), candies.end(), 0LL) < k) return 0;  // If total candies < k, return 0
 
-        int left = 1, right = *max_element(candies.begin(), candies.end());
-        int ans = 0;
+        int left=1;
+        int right=*max_element(candies.begin(), candies.end());
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        int ans=0;
 
-            if (canDistribute(candies, k, mid)) {
-                ans = mid;  // Try for a larger `mid`
-                left = mid + 1;
-            } else {
-                right = mid - 1;  // Reduce `mid` if not possible
+        auto valid=[&](int n){
+            long long count=0;
+            for(auto c: candies){
+                count+=c/n;
+            }
+            return count>=k; 
+        };
+
+        while(left<=right){
+            int mid=left + (right-left)/2;
+
+            if(valid(mid)){
+                ans=mid;
+                left=mid+1;
+            }
+            else{
+                right=mid-1;    
             }
         }
 
         return ans;
+        
     }
 };
