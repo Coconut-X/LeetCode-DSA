ac38752e773464d74dd8f9fa1a211503dbdcf188
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-03-19 23:11:34 +0500

    Time: 0 ms (100%), Space: 7.9 MB (19.78%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(long n) {
-        return n&&!(n&(n-1));
+
+        //return n&&!(n&(n-1));
+        if(n<=0) return false;
+        long int count = __builtin_popcount(n);
+        return (count==1 or count==0);
         
     }
 };
