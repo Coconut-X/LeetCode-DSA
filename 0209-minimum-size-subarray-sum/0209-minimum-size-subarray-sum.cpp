8ca8100ffcb28b92f5a2b928a4bf9cd6b95ce4b9
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-04-28 19:45:45 +0500

    Time: 39 ms (5.22%), Space: 47.3 MB (5.03%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -5,17 +5,12 @@ public:
 
     int minSubArrayLen(int target, vector<int>& nums) {
 
-        //O(nlogn) solution uwu
-
-        if(accumulate(nums.begin(),nums.end(),0) < target) return 0;
-
         unordered_map<int,int>prefix;
         prefix[0] = nums[0];
         prefix[-1] = 0;
-        cout<<prefix[0]<<" ";
+       
         for(int i=1; i<nums.size();i++){
             prefix[i] = prefix[i-1] + nums[i];
-            cout<<prefix[i]<<" ";
         }
         cout<<endl;
 
