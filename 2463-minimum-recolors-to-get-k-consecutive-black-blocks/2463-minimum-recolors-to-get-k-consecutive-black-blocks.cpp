c14d211c5fd4d2da1b619d064fbf10dee7ce462f
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-08-02 14:21:11 +0500

    Time: 0 ms (100%), Space: 8.3 MB (62.03%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    int minimumRecolors(string blocks, int k) 
-    {
-        int m = INT_MAX;
-        for (int i = 0; i <= blocks.size() - k; i++) {
-            
-            int count = 0;
-            for (int j = i; j < i + k; j++) {
-                if (blocks[j] == 'W') count++;
+    int minimumRecolors(string blocks, int k) {
+        int white = 0;
+        for (int i = 0; i < k; i++) {
+            if (blocks[i] == 'W') {
+                white++;
             }
-            m = min(m, count);
         }
+
+        int m = white;
+
+        for (int i = k; i < blocks.size(); i++) {
+            if (blocks[i - k] == 'W') {
+                white--;//[w,w,b,b,w,b] k=4, of at index i=4, remove w count at i=4 and increment count at index 4 to get W's count in next subarray 
+            }
+            if (blocks[i] == 'W') {
+                white++;
+            }
+            m = min(m, white);
+        }
+        
         return m;
     }
 };
