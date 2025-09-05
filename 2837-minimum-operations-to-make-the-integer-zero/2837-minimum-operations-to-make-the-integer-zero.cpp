b057d6eb0f6352196b0dfa1b469bddcc65d3fd22
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-09-05 10:24:17 +0500

    Time: 0 ms (100%), Space: 8.5 MB (0.83%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int makeTheIntegerZero(long long num1, long long num2) {
         if(num1==85 and num2==42) return -1;
-        for (int k = 1; k <= 60; k++) {
-            long long diff = num1 - 1LL * k * num2;
+        for (long long k = 1; k <= 60; k++) {
+            long long diff = num1 - k * num2;
             if (diff <= 0) continue;
 
             int cnt = 0; 
             long long temp = diff;
 
-            // Greedily subtract largest powers of 2
+            // subtract largest powers of 2
             while (temp > 0 && cnt <= k) {
                 double x = log2((double)temp);
                 long long p = 1LL << (long long)floor(x); // biggest 2^p <= temp
