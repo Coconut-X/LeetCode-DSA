8ab55f37c6fcc5b67d1319d8291b126a213115b0
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-09-08 00:41:04 +0500

    Time: 0 ms (100%), Space: 95.1 MB (80.14%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        int s1 = nums1.size(), s2 = nums2.size();
+
+        //always keep array A the smaller one, because we tune its index i based on j of array B
+        //to keep our complexity log(min(s1,s2))
+        if (s1 > s2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        int half = (s1+s2+1)/2;
+
+
+        long int left = 0, right = s1; //r = 4
+
+        int i = left + (right - left)/2; //start from middle. i = 2, use this formula to avoid overflow 
+        int j = half - i; // 4-2-1=2, initialy starts from same index as i
+        int Aleft, Aright;
+        int Bleft, Bright;
+        
+        while(left<=right){
+           
+            i = left + (right - left)/2; 
+            j = half - i;
+            
+            Aleft = (i == 0) ? INT_MIN : nums1[i-1];
+            Aright = (i == s1) ? INT_MAX : nums1[i];
+            Bleft = (j == 0) ? INT_MIN : nums2[j-1];
+            Bright = (j == s2) ? INT_MAX : nums2[j];
+
+            if(max(Aleft,Bleft)<=min(Aright,Bright)){
+                break;
+            }
+            //always move i and not j, beacuse j depends on i and will be updated on every loop
+            if(Aleft>Bright){
+                right = i-1;
+            }
+            if(Bleft>Aright){
+                left = i+1;
+            }
+        }
+
+        if((s1+s2)&1){
+            return max(Aleft,Bleft);
+        }
+
+        return (max(Aleft,Bleft)+min(Aright,Bright))/2.0;
+
+    }
+};
