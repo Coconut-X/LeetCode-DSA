0289721a059468faccb731c9c8a6b82c8171af43
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-09-11 12:53:11 +0500

    Time: 1672 ms (5.22%), Space: 16.6 MB (46.45%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -4,24 +4,27 @@ public:
         return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
     }
     string sortVowels(string s) {
-        // string t = "";
-        vector<int> freq(123,0);
+
         int n = s.size();
+        vector<int> buckets(10,0);
+        vector<char> vowels = {'A','E','I','O','U','a','e','i','o','u'};
+
+        unordered_map<int,int> mep;
+        mep['A'] = 0, mep['E'] = 1, mep['I'] = 2, mep['O'] = 3, mep['U'] = 4, mep['a'] = 5, mep['e'] = 6, mep['i'] = 7, mep['o'] = 8, mep['u'] = 9;
 
         for(int i  = 0; i < n; i++){
-            if(v(s[i])) freq[s[i]]++;
+            if(v(s[i])) buckets[mep[s[i]]]++;
         }
-
-        // sort(t.begin(),t.end());
         
-        for(int i = 0, j = 65; i < n; i++){
+        for(int i = 0, j = 0; i < n and j<=9; i++){
             if(v(s[i])){
-                while(freq[j]==0 and j <= 122){
+                while(buckets[j]==0 and j<=9){
                     j++;
                 }
-                if(j<=122 and freq[j]!=0){
-                    s[i] = j;
-                    freq[j]--;
+                if(j<=9 and buckets[j]!=0){
+                    s[i] = vowels[j];
+                    cout<<i<<" "<<j<<endl;
+                    buckets[j]--;
                 }
             }
         }
