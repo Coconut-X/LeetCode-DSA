45b82362a4c530f3bff3b285b25c52e6fd725c9f
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-09-11 12:05:18 +0500

    Time: 7 ms (89.74%), Space: 15.8 MB (78.32%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -4,18 +4,26 @@ public:
         return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
     }
     string sortVowels(string s) {
-        string t = "";
+        // string t = "";
+        vector<int> freq(123,0);
         int n = s.size();
 
         for(int i  = 0; i < n; i++){
-            if(v(s[i]))t+=s[i];
+            if(v(s[i])) freq[s[i]]++;
         }
 
-        sort(t.begin(),t.end());
+        // sort(t.begin(),t.end());
         
-        for(int i = 0, j = 0; i < n; i++){
-            if(v(s[i]))
-                s[i] = t[j++];
+        for(int i = 0, j = 65; i < n; i++){
+            if(v(s[i])){
+                while(freq[j]==0 and j <= 122){
+                    j++;
+                }
+                if(j<=122 and freq[j]!=0){
+                    s[i] = j;
+                    freq[j]--;
+                }
+            }
         }
 
         return s;
