ccd7084746b28303a04844151397f417a23fadf5
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-09-20 23:49:37 +0500

    Time: 8 ms (43.71%), Space: 18.7 MB (6.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
-        for(int i=0,j=i+1;i<nums.size()-1 and j<nums.size();j++)
-        {
-            if(i!=j and nums[i]+nums[j]==target) return {i,j};
-            if(j==nums.size()-1) i++,j=0;
+        unordered_map<int,bool> mep;
+        unordered_map<int,int>index;
+       // mep[-2,2,6] = true;
+        //index[]
+        for(int i = 1; i < nums.size(); i++){
+            mep[nums[i]-target] = true;
+            index[nums[i]-target] = i;
         }
-        return {0,0};
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(mep[-nums[i]]){
+
+                if(i == index[-nums[i]]){
+                    continue;
+                }
+                return {i, index[-nums[i]]};
+            }
+        }
+
+        return {};
+        
     }
 };
