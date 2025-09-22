f3d43f239ec8ab042339d07ead37ffc040418134
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-09-22 21:14:54 +0500

    Time: 0 ms (100%), Space: 23.2 MB (36.33%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -3,13 +3,16 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         vector<int>bucket(101, 0);
 
+        int maxelement = 0, minelement = 102;
         for(auto &n:nums){
+            maxelement = max(maxelement, n);
+            minelement = min(minelement,n);
             bucket[n]++;
         }
         int maxFreq = 0;
         int count = 0;
 
-        for(int i = 0; i <= 100; i++){
+        for(int i = minelement; i <= maxelement; i++){
             if(bucket[i] > maxFreq){
                 count = bucket[i];
                 maxFreq = bucket[i];
