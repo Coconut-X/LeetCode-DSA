6a8ae13cceb9819a66e73ad773d2099d320f5a2d
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-07 21:30:02 +0500

    Time: 3 ms (9.45%), Space: 19 MB (6%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,13 +1,28 @@
+struct pair_hash{
+    template<class T1, class T2>
+    size_t operator()(const pair<T1,T2>& p)const{
+        auto h1 = hash<T1>{}(p.first);
+        auto h2 = hash<T2>{}(p.second);
+        return h1 ^ (h2 << 1);
+    }
+};
+
 class Solution {
 public:
-    unordered_map<int,int> memo;
-    int minCostClimbingStairs(vector<int>& cost) {
-    int n=cost.size();
+    unordered_map<int,int>mep;
+    int climb(vector<int>&cost, int index){
+        if(index>=cost.size()){
+            return 0;
+        }
+       
+        if (mep.count(index)) return mep[index];
+       
+        int one = cost[index] + climb(cost, index+1);
+        int two = cost[index] + climb(cost, index+2);
+        return mep[index] = min(one,two);
+    }
 
-    for(int i=2;i<n;i++)
-        cost[i]+=min(cost[i-1],cost[i-2]);
-    
-    return min(cost[n-1], cost[n-2]);
-        
+    int minCostClimbingStairs(vector<int>& cost) {
+        return min(climb(cost,0), climb(cost,1));
     }
 };
