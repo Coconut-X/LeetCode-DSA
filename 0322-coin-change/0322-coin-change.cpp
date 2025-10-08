911d6814bdf898bb342561268603f6422efdaf54
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-08 21:13:20 +0500

    Time: 5 ms (99.39%), Space: 21.9 MB (53.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,16 @@
-struct pair_hash{
-    template<class T1, class T2>
-    size_t operator()(const pair<T1,T2>&p) const{
-        auto h1 = hash<T1>{}(p.first);
-        auto h2 = hash<T2>{}(p.second);
-        return h1 ^ (h2 << 1);
-    }
-};
-
 class Solution {
 public:
-    unordered_map<pair<int,int>,int, pair_hash> mep;
-    int brute(vector<int>&coins, int &amount, int index, long long current){
-        if(current==amount) return 0;
-
-        if(index==coins.size() or current>amount){
-            return INT_MAX -1;
-        }
-        if(mep.count({index,current}))return mep[{index,current}];
-        //include self again
-        int a = 1 + brute(coins,amount,index, current+coins[index]);
-        //skip
-        int b = brute(coins,amount,index+1, current);
-       
-        return mep[{index,current}] = min(a,b);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
-        
-        int ans = brute(coins,amount,0,0);
+        vector<long>dp(amount+1, INT_MAX);
+        dp[0] = 0;
 
-        if(ans>=INT_MAX-1) return -1;
-        return ans;
+        for(int i = 1; i <=amount; i++){
+            for(int j = 0; j< coins.size(); j++){
+                if(coins[j] <=i){
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                }
+            }
+        }
+        return dp[amount]==INT_MAX?-1:dp[amount];
     }
 };
