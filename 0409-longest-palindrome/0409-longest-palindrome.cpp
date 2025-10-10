10e6671c4d8bdd4bb91908f17eee781bf97fed0d
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-10 23:51:09 +0500

    Time: 0 ms (100%), Space: 9.5 MB (16.62%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int>freq;
+        vector<char> chars;
+        for(auto c:s){
+            if(!freq[c]){
+                chars.push_back(c);
+            }
+            freq[c]++;
+        }
+        int maxOdd = 0, maxEven = 0;
+        char odd = ' ';
+        for(auto c: chars){
+            if(freq[c]%2==1){ //if odd
+                if(freq[c]>=maxOdd){
+                    odd = c;
+                    maxOdd = freq[c];
+                }
+            }
+            else{
+                maxEven+=freq[c];
+            }
+        }
+        for(auto c:chars){
+            if(freq[c]%2==1 and c!=odd){
+                maxEven+=freq[c]-1;
+            }
+        }
+
+        return maxOdd+maxEven;
+    }
+};
