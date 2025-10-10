24ce5d3f99d4f3bd1ba2806e6be3fd9ed7a2cd47
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-11 00:19:53 +0500

    Time: 12 ms (42.35%), Space: 32.1 MB (54.65%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -4,9 +4,7 @@ public:
         sort(nums.begin(), nums.end());
         int s = 0;
         for(int i = 0; i < nums.size() - 1; i+=2)
-        {
             s+=min(nums[i], nums[i+1]);
-        }
         return s;
     }
 };
