eadd365cc9f4a8110615f7f99533a5d7823b1370
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-15 20:35:08 +0500

    Time: 0 ms (100%), Space: 36.2 MB (96.67%) - LeetHub

--- 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.cpp
+++ 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     bool canThreePartsEqualSum(vector<int>& arr) {
         int sum = accumulate(arr.begin(), arr.end(), 0);
-        cout<<"Sum: "<<sum<<endl;
-        int part = sum/3;
         
         if(sum%3!=0) return false;
-        //if(part!=test) return false;
-        cout<<"hi"<<endl;
+        int part = sum/3; //sum of one part
         int low = 0, high = arr.size()-1;
         int sleft = 0, sright=0;
+        //for arrays like [0,0,0,] [-1,1,1,-1] the part will be zero 
+        //ans initially both sleft and sright were zero so it will return true on very first 
+        //if statement in while loop, so we add first numbers to them move both indices
         if(part==0) sleft+=arr[low++], sright+=arr[high--];
+
         while(low<=high){
             
             if(sleft==part and sright==part) return true;
@@ -21,7 +22,6 @@ public:
             if(sright!=part){
                 sright+=arr[high--];
             }
-            cout<<sleft<<"  "<<sright<<endl;
         }
         return false;
     }
