7f557bb6607aa9ba4d81570306be234365f08de8
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-15 22:03:43 +0500

    Time: 3 ms (24.26%), Space: 14.4 MB (50.34%) - LeetHub

---
+++ 1519-minimum-subsequence-in-non-increasing-order/1519-minimum-subsequence-in-non-increasing-order.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> minSubsequence(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int total = accumulate(nums.begin(), nums.end(),0);
+        int s = 0;
+        vector<int>ans;
+        for(int i = nums.size() - 1; i >=0; i--){
+            s+=nums[i];
+            ans.push_back(nums[i]);
+            if(2*s> total) break;
+        }
+        return ans;
+    }
+};
