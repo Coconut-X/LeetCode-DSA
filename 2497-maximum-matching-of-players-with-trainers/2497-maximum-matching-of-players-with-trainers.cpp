435a1f5b7589234756192c756834e253cb9a535c
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-15 14:33:27 +0500

    Time: 47 ms (5.04%), Space: 80.3 MB (75.45%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
 
-        unordered_map<int, bool> mep;
         int count = 0;
+        sort(players.begin(), players.end());
         sort(trainers.begin(), trainers.end());
 
         for(int i = 0; i< players.size(); i++){
-            
             auto it = lower_bound(trainers.begin(), trainers.end(), players[i]);
-            
-            if(it!=trainers.end()){
-                count++;
-                trainers.erase(it);
-            }
-
+            if(it==trainers.end()) continue;
+            count++;
+            trainers[it - trainers.begin()] =  -1;
         }
-        
         return count;
     }
 };
