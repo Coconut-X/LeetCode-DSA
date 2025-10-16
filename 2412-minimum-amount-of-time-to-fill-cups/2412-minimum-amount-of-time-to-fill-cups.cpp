e1c7b59a3d4eec015e4ee965a89d1b119c8a0a52
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-16 21:02:38 +0500

    Time: 0 ms (100%), Space: 14 MB (81.89%) - LeetHub

--- 2412-minimum-amount-of-time-to-fill-cups/2412-minimum-amount-of-time-to-fill-cups.cpp
+++ 2412-minimum-amount-of-time-to-fill-cups/2412-minimum-amount-of-time-to-fill-cups.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int fillCups(vector<int>& amount) {
         int time = 0;
-        //sort(amount.begin(),amount.end());
         int a = amount[0], b = amount[1], c = amount [2];
         while(a or b or c){
+            //if only one type of water is left return time + remaining water 
             if(!a and !b and c) return time +c;
             if(!a and b and !c) return time +b;
             if(a and !b and !c) return time +a;
 
+            //pick the two greatest
+            //if one water is zero it will never be chosed because other two will be non zero
+            
             if(a>=b and c>=b) amount[0]--, amount[2]--, time++;
             else if( b>=a and c>=a) amount[1]--, amount[2]--, time++;
             else if(a>=c and b>=c) amount[0]--, amount[1]--, time++;
