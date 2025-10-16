2e99c2d35d4c434446e43dcc2b212bfad1a4b3c4
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-16 17:57:37 +0500

    Time: 0 ms (100%), Space: 8 MB (81.3%) - LeetHub

---
+++ 2650-split-with-minimum-sum/2650-split-with-minimum-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int splitNum(int num) {
+        string n = to_string(num);
+        sort(n.begin(), n.end());
+        string left = "", right = "";
+
+        for(int i = 0; i < n.size(); i++){
+            if(i%2==0) left+=n[i];
+            else right+=n[i];
+        }
+        cout<<left<<" "<<right;
+        return stoi(right) + stoi(left);
+    }
+};
