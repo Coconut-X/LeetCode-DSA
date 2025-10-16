f9b138c88f70c4eb9df25d8fe702b97ea847d957
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-16 18:43:31 +0500

    Time: 0 ms (100%), Space: 76.6 MB (37.5%) - LeetHub

--- 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.cpp
+++ 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.cpp
@@ -5,9 +5,14 @@ public:
         // 1  2  3  4   5    6   7 
         // 1  3  6  10  15  21   28
         // 28-10
-        int left = (m)*(m-1)/2;
-        int right = (m+k)*(m+k-1)/2;
-        //cout<<left<<" "<<right;
-        return right - left;
+        //int left = (m)*(m-1)/2,     right = (m+k)*(m+k-1)/2;
+        //return ((m+k)*(m+k-1) - (m)*(m-1))/2;
+        //mm+mk -m  mk +kk -k   -mm +m
+        //mk +  mk +kk -k  
+        //2mk +kk -k => k(2m+k-1) => k(2m+k-1)/2 => k*(m << 1 + k - 1) >> 1
+        //or divide by two each term we get mk +kk/2 -k/2 => mk + k(k-1)/2  => m*k + (k*(k-1) >> 1)
+        //this one is better so imma use this
+        return m*k + (k*(k-1) >> 1);
+        //return k*((m << 1) + k - 1) >> 1;
     }
 };
