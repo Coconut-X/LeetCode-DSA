d26ed2b70957cb72995285f350e39503beb5faf8
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-16 17:35:52 +0500

    Time: 0 ms (100%), Space: 14.1 MB (77.84%) - LeetHub

--- 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.cpp
@@ -3,8 +3,7 @@ public:
     int minimumCost(vector<int>& cost) {
         sort(cost.begin(), cost.end(), greater<int>());
         int mincost = 0;
-        for(int i = 0; i < cost.size(); i++)
-            mincost+=((i+1)%3==0?0:cost[i]);
+        for(int i = 0; i < cost.size(); i++) mincost+=((i+1)%3==0?0:cost[i]);
         return mincost;
     }
 };
