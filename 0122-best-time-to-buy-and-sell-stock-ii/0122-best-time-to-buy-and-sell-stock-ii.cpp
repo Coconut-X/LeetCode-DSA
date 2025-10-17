d679f10a8b658a6dee6cb499694b943f8703554e
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-18 00:14:14 +0500

    Time: 0 ms (100%), Space: 17.2 MB (44.69%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0, s = prices.size(), i = 0;
+        int profit = 0;
+        //1 2 3 4
+        int s = prices.size();
+        int i = 0;
         while(i < s){
-            while(i+1!=s and prices[i+1]<= prices[i]) i++; //skip downhill
+            int curr = prices[i];
+            //cout<<"Current: "<<curr<<endl;
+            
+            //using i < s will make us advance to the next index due to one extra increment of i to break for loop
+            while(i+1!=s and prices[i+1]<= prices[i]){ ////skip the downfall
+                i++; 
+            }
             if(i>=s) return profit; //nothing bought
-            int buy = prices[i]; 
-            while(i+1!=s and prices[i+1]>=prices[i]) i++; //now find the next peak
-            if(i>=s) return profit; //nothing sold
-            profit +=prices[i++]-buy;
+
+            //else this point is maybe end of array of turning point or a plateaue
+            int buy = prices[i]; //we can also select first index with this one
+            //cout<<"Buying at: "<<buy<<endl;
+
+            //now find the next peak
+            while(i+1!=s and prices[i+1]>=prices[i]){
+                i++;
+            }
+            if(i>=s) return profit; //dont remove current buy because we couldnt sell it so we assume we didnt buy it
+            int sold = prices[i];
+            //cout<<"Selling at: "<<sold<<endl;
+            profit +=sold-buy;
+            //cout<<profit<<endl;
+            i++;
         }
         return profit;
     }
