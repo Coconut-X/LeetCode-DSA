8ba64d3b47768531cf70f464e810f20afa15a918
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-19 00:32:39 +0500

    Time: 4 ms (65.66%), Space: 7.8 MB (85.05%) - LeetHub

--- 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
@@ -8,7 +8,7 @@ public:
             if(!a and b and !c) return score ;
             if(a and !b and !c) return score ;
 
-            //if one water is zero it will never be chosen because other two will be non zero
+            //if one pile is zero it will never be chosen because other two will be non zero
             //pick the two greatest, remove them by one from their pile and increment score
             
             if(a>=b and c>=b) a--, c--, score++;
