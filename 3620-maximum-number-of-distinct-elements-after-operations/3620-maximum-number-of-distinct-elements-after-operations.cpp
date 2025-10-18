62893fe39f2f7c3d3e8572559afd9e38f5160487
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-18 11:01:36 +0500

    Time: 122 ms (52.68%), Space: 97.6 MB (88.33%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -2,13 +2,8 @@ class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        unordered_set<int>unique;
-        nums[0] = nums[0] - k; ////min range possible
-        unique.insert(nums[0]);        
-        for(int i = 1; i < nums.size(); i++){
-            nums[i] = (max(nums[i-1]+1, nums[i] - k) > nums[i] + k )? nums[i] + k: max(nums[i - 1]+1, nums[i] - k);
-            unique.insert(nums[i]);        
-        }
-        return unique.size();
+        int index = 1;
+        for(int i = 0; i < nums.size(); i++)  nums[i] = i==0 ?nums[i] - k: (max(nums[i-1]+1, nums[i] - k) > nums[i] + k )? nums[i] + k : max(nums[i - 1]+1, nums[i] - k) + ++index - index;
+        return index;
     }
 };
