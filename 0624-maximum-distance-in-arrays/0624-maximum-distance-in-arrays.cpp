36edf08b6b4c7003b90d54199cc80a24bf10ff0c
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-19 02:35:07 +0500

    Time: 0 ms (100%), Space: 108.2 MB (20.04%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -3,12 +3,16 @@ public:
     int maxDistance(vector<vector<int>>& a) {
         int min1 = INT_MAX, min2 = INT_MAX, max1 = INT_MIN, max2 = INT_MAX;
         int minrow, maxrow;
-        
+
         for(int i = 0; i < a.size(); i++){
-            if(a[i][0] < min1) min2 = min1, min1 = a[i][0], minrow = i;
-            else if(a[i][0] < min2) min2 = a[i][0];
-            if(a[i][a[i].size() - 1] > max1) max2 = max1, max1 = a[i][a[i].size() - 1], maxrow = i;
-            else if(a[i][a[i].size() - 1] > max2) max2 = a[i][a[i].size() - 1];
+            int first = a[i][0], last = a[i].back();
+            //find first and second smallert
+            if(first < min1) min2 = min1, min1 = first, minrow = i;
+            else if(first < min2) min2 = first;
+
+            //find first and second largest
+            if(last > max1) max2 = max1, max1 = last, maxrow = i;
+            else if(last > max2) max2 = last;
         }
 
         if(minrow != maxrow) return max1 - min1;
