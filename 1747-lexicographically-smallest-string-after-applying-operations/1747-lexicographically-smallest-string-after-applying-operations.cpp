31089f8bb637fb2deeeb3dd4e7d2fd04f88e111c
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-10-19 16:25:56 +0500

    Time: 228 ms (57.52%), Space: 119.3 MB (13.27%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,38 +1,33 @@
-struct tuple_hash {
-    size_t operator()(const tuple<string,int,int>& t) const {
-        auto h1 = hash<string>{}(get<0>(t));
-        auto h2 = hash<int>{}(get<1>(t));
-        auto h3 = hash<int>{}(get<2>(t));
-        return h1 ^ (h2 << 1) ^ (h3 << 2);
-    }
-};
-
 class Solution {
 public:
-    unordered_map<string,bool>mep;
-    string brute(const string& s, int a, int b) {
-        if(mep.count(s)) return s;
-        mep[s] = true;
-        string res = s;
-
-        int len = s.size();
-        int shift = b % len;
-        string rot = s;
-        if(shift > 0) {
-            rot = s.substr(len - shift) + s.substr(0, len - shift);
-            res = min(res, brute(rot, a, b));
-        }
-
-        string addA = s;
-        for(int i = 1; i < len; i += 2) {
-            addA[i] = (addA[i] - '0' + a) % 10 + '0';
-        }
-        if(addA != s) res = min(res, brute(addA, a, b));
-
-        return res;
-    }
-
     string findLexSmallestString(string s, int a, int b) {
-        return brute(s, a, b);
+        
+        unordered_map<string,bool> v;
+        queue<string>q;
+        q.push(s);
+        v[s] = true;
+        int len = s.size();
+        string m = s;
+        while(!q.empty()){
+            string top = q.front();
+            q.pop();
+            m = min(m,top);
+            string rot = top;
+            int shift = b % len;
+            if(shift > 0) rot = top.substr(len - shift) + top.substr(0, len - shift);
+
+            string add = top;
+            for(int i = 1; i < len; i+=2) add[i]  = ((add[i]-'0'+a)%10) +'0';
+
+            if(!v[rot]){
+                v[rot] = true;
+                q.push(rot);
+            } 
+            if(!v[add]){
+                v[add] = true;
+                q.push(add);
+            } 
+        }
+        return m;
     }
 };
