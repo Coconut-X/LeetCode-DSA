d64a98615e2c38f037ef5b2a2558ba7436c10e7e
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-11-01 00:44:20 +0500

    Time: 0 ms (100%), Space: 25.3 MB (75.74%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
         vector<int>sneaky;
-        unordered_map<int,bool>mep;
+        bitset<100>mep;
+        
         int s = nums.size();
-        for(int i = 0; i < s; i++)
-        {
-            if(mep[nums[i]])sneaky.push_back(nums[i]);
-            mep[nums[i]]= true;
-            
+        for(int i = 0; i < s; i++){
+            if(mep.test(nums[i])) sneaky.push_back(nums[i]);
+            mep.set(nums[i]);  
         }
         return sneaky;
     }
