7291916a2a3deb6b05c567c646995d417dfebadb
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-11-04 17:58:11 +0500

    Time: 31 ms (11.5%), Space: 37.9 MB (14.7%) - LeetHub

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    int sub(vector<int>&nums, int k, int xx, int x){
+        unordered_map<int,int> freq;
+        priority_queue<pair<int,int>> pq;
+
+        for(int i = x; i < x + k; i++) freq[nums[i]]++;
+        for(int i = x; i < x + k; i++) pq.push({freq[nums[i]],nums[i]});
+
+        int t = 1;
+        int sum = 0;
+
+        while(t<=xx and !pq.empty()){
+
+            pair<int,int> pqtop = pq.top();
+            pq.pop();
+
+            while(!pq.empty() and pq.top().first==pqtop.first && pq.top().second == pqtop.second){ //if same frequency
+                if(pq.top().second > pqtop.second){ //choose bigger nunmber
+                    pqtop = pq.top();
+                }
+                pq.pop();
+            }
+            
+            sum+=pqtop.second*pqtop.first; //number*frequency
+            t++;
+        }
+
+        return sum;
+    }
+
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        vector<int>topx;
+
+        for(int i = 0; i <= nums.size() - k ; i++){
+            topx.push_back(sub(nums,k,x,i));
+        }
+        
+        return topx;
+    }
+};
