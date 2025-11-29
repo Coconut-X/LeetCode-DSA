3da414d4d93932687182e5499e39674dd18adc82
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2025-11-29 10:34:02 +0500

    Time: 0 ms (100%), Space: 45.3 MB (35.3%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        vector<int>mods (nums.size(),0);
-        int sum = 0;
-
-        for(int i = 0; i < nums.size(); i++){
-            sum+=nums[i];
-            mods[i] = nums[i]%k;
-        }
-
-        sort(mods.begin(), mods.end(), greater<int>());
-
-
-        int count = 0;
-        int i = 0;
-        while(sum%k){
-            if(mods[i] = sum%k){
-                count+=mods[i];
-                sum -= mods[i];
-            }
-        }
-        return count;
+        return accumulate(nums.begin(), nums.end(), 0) % k;
     }
 };
