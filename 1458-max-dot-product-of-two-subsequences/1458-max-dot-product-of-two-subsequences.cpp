0b11a8383bd97c5b2724bcf78b9b4049c477c3db
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2026-01-09 00:23:23 +0500

    Time: 43 ms (21.6%), Space: 19 MB (18.8%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -4,8 +4,7 @@ public:
     int brute(vector<int>&n1, vector<int>&n2, int i, int j, vector<vector<int>>&dp){
         if(i >= n1.size() or j >= n2.size()) return INT_MIN;
         if(dp[i][j]!=INT_MIN) return dp[i][j];
-        dp[i][j] = max({n1[i]*n2[j] + max(0, brute(n1,n2,i+1,j+1,dp)), brute(n1,n2,i+1,j,dp), brute(n1,n2,i,j+1,dp)});
-        return dp[i][j];
+        return dp[i][j] = max({n1[i]*n2[j] + max(0, brute(n1,n2,i+1,j+1,dp)), brute(n1,n2,i+1,j,dp), brute(n1,n2,i,j+1,dp)});
     }
 
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
