8dcfa6fca110b68e0ffc1d0f41870ebee4ab5d3d
Ali Imran <144850519+Coconut-X@users.noreply.github.com>
2026-04-24 14:22:18 +0500

    Time: 0 ms (100%), Space: 8.9 MB (90.95%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int l = count(moves.begin(), moves.end(), 'L');
+        int r = count(moves.begin(), moves.end(), 'R');
+        int us = count(moves.begin(), moves.end(), '_');
+
+        return abs(l - r) + us;
+
+    }
+};
